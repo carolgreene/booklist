00e11d392f02777fdfbe8dd4288a1297a41d9ee4
carolgreene <carolgreene3@gmail.com>
2020-05-07 18:11:40 -0700

    added validation and alerts

--- app.js
+++ app.js
@@ -21,7 +21,28 @@ function UI() {}
       <td><a href="#" class="delete">X</a></td>
     `
     list.appendChild(row)
+  }
 
+  // Show alert
+  UI.prototype.showAlert = function(message, className) {
+    //create div
+    const div = document.createElement('div');
+    //add classes
+    div.className = `alert ${className}`;
+    //add text
+    div.appendChild(document.createTextNode(message))
+    //get parent
+    const container = document.querySelector('.container');
+    //get form
+    const form = document.querySelector('#book-form');
+    //insert alert
+    container.insertBefore(div, form)
+
+    //Timeout after 3 sec
+    setTimeout(function() {
+      document.querySelector('.alert')
+      .remove();
+    }, 3000)
   }
 
   //Clear Fields
@@ -47,14 +68,22 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
   // Instantiate UI
   const ui = new UI()
 
-  console.log(ui)
+  //validate
+  if(title === '' || author === '' || isbn === '') {
+    //Error alert
+    ui.showAlert('Please fill in all fields', 'error');
+  } else {
+    //add book to list
+    ui.addBookToList(book);
+    //clear fields
+    ui.clearFields()
+  
+    //show success
+    ui.showAlert('Book Added!', 'success')
 
-  //add book to list
-  ui.addBookToList(book)
-
-  //clear fields
-  ui.clearFields()
-
+    //clear fields
+    ui.clearFields()
+  }
   
   e.preventDefault();
 })
